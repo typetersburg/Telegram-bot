b1aa7825d5f6efbd03ae3cc8df9674cacaf0ca82
Maksim Sadakov <pm@msadakov.me>
2019-03-27 08:04:39 +0300

    Add GetChanWithUpdates for Tg bot API
    GetChanWithUpdates receives updates from the Telegram API, or updates
    come through the webhook.

--- tg/tg.go
+++ tg/tg.go
@@ -1,6 +1,8 @@
 package tg
 
 import (
+	"net/http"
+
 	tg "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/pkg/errors"
 )
@@ -19,4 +21,38 @@ func (c Config) New() (*tg.BotAPI, error) {
 	bot.Debug = c.Debug
 
 	return bot, nil
+}
+
+// GetChanWithUpdates receives updates from the Telegram API, or updates come
+// through the webhook.
+func (c Config) GetChanWithUpdates(bot *tg.BotAPI) (<-chan tg.Update, error) {
+	// Remove webhook if already crated
+	if _, err := bot.RemoveWebhook(); err != nil {
+		return nil, errors.Wrap(err, "can't remove webhook")
+	}
+
+	// If the bot will be launched using update retrieval, not via webhook.
+	if !c.isUseTgWebhooks() {
+		upd := tg.NewUpdate(0)
+		upd.Timeout = c.UpdateTimeout
+		upd.Limit = c.UpdateLimit
+		return bot.GetUpdatesChan(upd)
+	}
+
+	webhookConfig := tg.NewWebhook(c.WebhookURL + c.Token)
+	webhookConfig.MaxConnections = c.WebhookMaxConnections
+
+	_, err := bot.SetWebhook(webhookConfig)
+	if err != nil {
+		return nil, errors.Wrap(err, "can't set webhook "+c.WebhookURL)
+	}
+
+	go func() {
+		err = http.ListenAndServe(c.WebhookHostname, nil)
+	}()
+	if err != nil {
+		return nil, errors.Wrap(err, "can't init server "+c.WebhookHostname)
+	}
+
+	return bot.ListenForWebhook("/" + bot.Token), nil
 }
