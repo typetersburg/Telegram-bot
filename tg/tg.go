67ba3540686622f022f62f4d2c83feb84a7da50e
Maksim Sadakov <pm@msadakov.me>
2019-04-14 14:17:51 +0300

    Add message worker pool

--- tg/tg.go
+++ tg/tg.go
@@ -1,7 +1,10 @@
 package tg
 
 import (
+	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	tg "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/pkg/errors"
@@ -55,4 +58,63 @@ func (c Config) GetChanWithUpdates(bot *tg.BotAPI) (<-chan tg.Update, error) {
 	}
 
 	return bot.ListenForWebhook("/" + bot.Token), nil
+}
+
+// workerContext provides context for workers.
+type workerContext struct {
+	id  int
+	wg  *sync.WaitGroup
+	bot *tg.BotAPI
+}
+
+// InitMessageWorkerPool creates a pool of workers who send messages to
+// Telegram based on the API limit.
+func (c Config) InitMessageWorkerPool(bot *tg.BotAPI) (
+	*sync.WaitGroup, chan<- tg.Chattable) {
+	// limiter to limit sending messages so that Telegram does not reject messages.
+	limitTicker := time.NewTicker(time.Second / messagesPerSecondTgLimit)
+
+	// Wait group for graceful shutdown.
+	wg := &sync.WaitGroup{}
+	wg.Add(c.MessageWorkers)
+
+	workerCtx := workerContext{
+		wg:  wg,
+		bot: bot,
+	}
+
+	// Channel for sending messages.
+	ch := make(chan tg.Chattable, messagesPerSecondTgLimit)
+
+	for i := 0; i < c.MessageWorkers; i++ {
+		workerCtx.id = i
+		go messageWorker(workerCtx, ch, limitTicker)
+	}
+
+	return wg, ch
+}
+
+func messageWorker(
+	ctx workerContext,
+	msgCh <-chan tg.Chattable,
+	limitTicker *time.Ticker,
+) {
+	defer ctx.wg.Done()
+	for {
+		c, ok := <-msgCh
+		// Check if the channel is closed. The channel closes when the
+		// application ends.
+		if !ok {
+			log.Printf("[TG][DEBUG] worker %d closed\n", ctx.id)
+			return
+		}
+
+		// Rate limiter
+		<-limitTicker.C
+
+		_, err := ctx.bot.Send(c)
+		if err != nil {
+			log.Printf("[TG][ERROR] can't send message: %v\n", err)
+		}
+	}
 }
